320f028a8359c8e2a373b3a8eba8f1d5ebee540b
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2018-07-19 16:01:25 +0100

    Updated calendar service. Cleanup

--- src/main/java/org/lab/insurance/common/controller/HolidayCalendarController.java
+++ src/main/java/org/lab/insurance/common/controller/HolidayCalendarController.java
@@ -2,11 +2,14 @@ package org.lab.insurance.common.controller;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+import org.lab.insurance.common.exception.InsuranceCommonException;
 import org.lab.insurance.common.model.HolidayCalendar;
 import org.lab.insurance.common.repository.HolidayCalendarRepository;
 import org.lab.insurance.common.services.HolidayCalendarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,19 +33,24 @@ public class HolidayCalendarController {
 
 	@GetMapping("/{id}")
 	public ResponseEntity<HolidayCalendar> find(@PathVariable String id) {
-		return ResponseEntity.ok(repository.findById(id).get());
+		Optional<HolidayCalendar> optional = repository.findById(id);
+		return optional.isPresent() ? ResponseEntity.ok(optional.get()) : new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
 	@PostMapping("/next-laboral")
 	public LocalDate nextLaboralDay(@RequestBody NextLaboralDayRequest request) {
 		LocalDate localDate = LocalDate.from(request.getFrom());
-		return calendarService.getNextLaboralDay(localDate, request.getDays(), request.getCalendarId());
+		HolidayCalendar calendar = repository.findById(request.getCalendarId()).orElseThrow(
+			() -> new InsuranceCommonException("Invalid calendar identifier '" + request.getCalendarId() + "'"));
+		return calendarService.getNextLaboralDay(localDate, request.getDays(), calendar);
 	}
 
 	@PostMapping("/check-laboral")
 	public Boolean checkLaboralDay(@RequestBody CheckLaboralDayRequest request) {
 		LocalDate localDate = LocalDate.from(request.getFrom());
-		return calendarService.isLaboralDay(localDate, request.getCalendarId());
+		HolidayCalendar calendar = repository.findById(request.getCalendarId()).orElseThrow(
+			() -> new InsuranceCommonException("Invalid calendar identifier '" + request.getCalendarId() + "'"));
+		return calendarService.isLaboralDay(localDate, calendar);
 	}
 
 	@Getter
--- src/main/java/org/lab/insurance/common/services/HolidayCalendarService.java
+++ src/main/java/org/lab/insurance/common/services/HolidayCalendarService.java
@@ -2,12 +2,13 @@ package org.lab.insurance.common.services;
 
 import java.time.LocalDate;
 
+import org.lab.insurance.common.model.HolidayCalendar;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HolidayCalendarService {
 
-	public LocalDate getNextLaboralDay(LocalDate date, int days, String calendarId) {
+	public LocalDate getNextLaboralDay(LocalDate date, int days, HolidayCalendar calendar) {
 		LocalDate tmp = LocalDate.from(date);
 
 		// Note: in the case of previous dates we want to have an offset
@@ -15,7 +16,7 @@ public class HolidayCalendarService {
 		int count = 0;
 		int increment = days < 0 ? -1 : 1;
 		do {
-			if (!isLaboralDay(tmp, calendarId)) {
+			if (!isLaboralDay(tmp, calendar)) {
 				tmp = tmp.plusDays(increment);
 				continue;
 			}
@@ -26,13 +27,16 @@ public class HolidayCalendarService {
 		return tmp;
 	}
 
-	public boolean isLaboralDay(LocalDate date, String calendarId) {
+	public boolean isLaboralDay(LocalDate date, HolidayCalendar calendar) {
 		// TODO check holiday
 		switch (date.getDayOfWeek()) {
 		case SATURDAY:
 		case SUNDAY:
 			return false;
 		default:
+			if (calendar.getDates().stream().filter(x -> x.getDate().equals(date)).findAny().isPresent()) {
+				return false;
+			}
 			return true;
 		}
 	}
